df6424bc02805eee78234adb1a760a7308103db8
Victor <vklee88@berkeley.edu>
2018-03-24 16:35:32 -0700

    Front end skeleton for user route planning

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ var indexRouter = require('./routes/index');
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, 'public/views'));
 app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
--- public/javascripts/app_controller.js
+++ public/javascripts/app_controller.js
@@ -3,3 +3,16 @@ var app = angular.module('VoltaPlanner', []);
 app.controller('mainController', function($http, $scope) {
 
 });
+
+function initMap() {
+    var map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: 37.7749, lng: -122.4194},
+        zoom: 10
+    });
+
+    var timeout;
+    var startInput = document.getElementById('start_location');
+    var startAutocomplete = new google.maps.places.Autocomplete(startInput, {placeIdOnly: true});
+    var endInput = document.getElementById('end_location');
+    var endAutocomplete = new google.maps.places.Autocomplete(endInput, {placeIdOnly: true});
+}
