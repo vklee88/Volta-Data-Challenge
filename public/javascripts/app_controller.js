de65c41f5bc96ff53006a0392a729e2e00050a25
Victor <vklee88@berkeley.edu>
2018-03-24 17:17:55 -0700

    frontend skeleton to send and receive data to backend

--- public/javascripts/app_controller.js
+++ public/javascripts/app_controller.js
@@ -1,7 +1,45 @@
 var app = angular.module('VoltaPlanner', []);
 
 app.controller('mainController', function($http, $scope) {
+    $scope.planTrip = function() {
+        var startLocation = document.getElementById('start_location');
+        var endLocation = document.getElementById('end_location');
+        var geocoder = new google.maps.Geocoder();
+        geocoder.geocode({'placeId': startLocation}, function(startCoord, status) {
+            if (status !== 'OK') {
+                alert('Geocode failed for the operation');
+                return;
+            }
+            geocoder.geocode({'placeId': endLocation}, function(endCoord, status) {
+                if(status === 'OK') {
+                    $http.get('/route?startLocation=' + startCoord + '&endLocation=' + endCoord)
+                        .success(function (res) {
+                            var map = new google.maps.Map(document.getElementById('map'), {
+                                zoom: 3,
+                                center: {lat: 0, lng: -180},
+                                mapTypeId: 'terrain'
+                            });
 
+                            var flightPlanCoordinates = res.coordList;
+                            var flightPath = new google.maps.Polyline({
+                                path: flightPlanCoordinates,
+                                geodesic: true,
+                                strokeColor: '#FF0000',
+                                strokeOpacity: 1.0,
+                                strokeWeight: 2
+                            });
+
+                            flightPath.setMap(map);
+                        })
+                        .failure(function (err) {
+                            alert(err);
+                        })
+                } else {
+                    alert('Geocode failed for the operation');
+                }
+            });
+        });
+    }
 });
 
 function initMap() {
--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,12 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Volta Trip Planning' });
+router.get('/', function(req, res) {
+    res.render('index', { title: 'Volta Trip Planning' });
+});
+
+router.get('/route', function(req, res) {
+
 });
 
 module.exports = router;
